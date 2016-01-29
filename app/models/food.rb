b6386cb17230dddedf182d3b581b0c8434094282
Miguel Wilson Cruz <esaas@navegawireless.com>
2016-01-29 16:12:32 -0500

    commit before merging rel1

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,18 @@
 class Food < ActiveRecord::Base
+<<<<<<< HEAD
+=======
+	self.primary_key = 'nutrient_databank_number'
+	validates :food_group_code, presence: true, uniqueness: true
+    validates :long_description, presence: true
+    validates :short_description, presence: true
+    validates :nutrient_databank_number, presence: true, uniqueness: true
+
+    belongs_to :foodgroup, class_name: 'Foodgroup', foreign_key: 'food_group_code'
+    
+    has_many :nutrients, class_name: 'Nutrient', foreign_key: 'nutrient_databank_number'
+
+    has_many :nutrient_definitions, class_name: 'NutrientDefinition', 
+          through: :nutrients
+
+>>>>>>> edge22
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160108041043) do
 
   create_table "foods", force: :cascade do |t|
@@ -19,6 +20,25 @@ ActiveRecord::Schema.define(version: 20160108041043) do
     t.text     "long_description"
     t.text     "short_description"
     t.string   "common_name"
+=======
+ActiveRecord::Schema.define(version: 20160126002940) do
+
+  create_table "foodgroups", id: false, force: :cascade do |t|
+    t.string   "food_group_code",        null: false
+    t.text     "food_group_description", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "foodgroups", ["food_group_code"], name: "index_foodgroups_on_food_group_code"
+
+  create_table "foods", id: false, force: :cascade do |t|
+    t.string   "nutrient_databank_number", null: false
+    t.string   "food_group_code",          null: false
+    t.text     "long_description",         null: false
+    t.string   "short_description",        null: false
+    t.text     "common_name"
+>>>>>>> edge22
     t.string   "manufacturer"
     t.string   "survey"
     t.text     "refuse_description"
@@ -28,11 +48,56 @@ ActiveRecord::Schema.define(version: 20160108041043) do
     t.decimal  "protein_factor"
     t.decimal  "fat_factor"
     t.decimal  "carbohydrate_factor"
+<<<<<<< HEAD
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
 
   add_index "foods", ["food_group_code_id"], name: "index_foods_on_food_group_code_id"
   add_index "foods", ["nutrient_databank_id"], name: "index_foods_on_nutrient_databank_id"
+=======
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "foods", ["food_group_code"], name: "index_foods_on_food_group_code"
+
+  create_table "nutrient_definitions", id: false, force: :cascade do |t|
+    t.string  "nutrient_number",       null: false
+    t.string  "units",                 null: false
+    t.string  "tagname"
+    t.string  "nutrient_description",  null: false
+    t.string  "number_decimal_places", null: false
+    t.integer "sort_record_order",     null: false
+  end
+
+  add_index "nutrient_definitions", ["nutrient_number"], name: "index_nutrient_definitions_on_nutrient_number"
+
+  create_table "nutrients", id: false, force: :cascade do |t|
+    t.string   "nutrient_databank_number", null: false
+    t.string   "nutrient_number",          null: false
+    t.decimal  "nutrient_value",           null: false
+    t.decimal  "num_data_points",          null: false
+    t.decimal  "std_err"
+    t.string   "source_code",              null: false
+    t.string   "data_deriv_code"
+    t.string   "ref_ndb_no"
+    t.string   "added_nutr_mark"
+    t.integer  "num_studies"
+    t.decimal  "min"
+    t.decimal  "max"
+    t.decimal  "degrees_of_freedom"
+    t.decimal  "low_eb"
+    t.decimal  "up_eb"
+    t.string   "statistical_comment"
+    t.string   "addmod_date"
+    t.string   "confidence_code"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "nutrients", ["nutrient_databank_number"], name: "index_nutrients_on_nutrient_databank_number"
+  add_index "nutrients", ["nutrient_number"], name: "index_nutrients_on_nutrient_number"
+>>>>>>> edge22
 
 end
